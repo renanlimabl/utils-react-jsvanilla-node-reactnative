44a6ef5e4a618ec888a4bd6af039f9adaecc6de9
Renan Lima <renanlimabl@gmail.com>
2021-01-31 19:15:39 -0300

    add effectsBasics jquery

---
+++ comebackToJquery/effetsBasic.js
@@ -0,0 +1,20 @@
+/**
+ * .hide() @params normal, fast, slow, e ms
+ * Desaparece com o elemento selecionado,
+ * Ex:
+ * $('.exemplo').hide(3000)
+ */
+
+/**
+ * .show() @params normal, fast, slow, e ms
+ * Aparece um elemento selecionado.
+ * Ex:
+ * $('.exemplo').show(normal);
+ */
+
+/**
+ * .toggle() @params normal, fast, slow, e ms
+ * Verifica se está hide, se estiver hide ele mostra, se estiver show ele oculta.
+ * $('.exemplo').toggle(fast)
+*/
+
