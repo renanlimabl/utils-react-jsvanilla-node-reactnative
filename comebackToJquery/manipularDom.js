ce8b515101a17d2f80cee07865a9cfab973b3461
Renan Lima <renanlimabl@gmail.com>
2021-01-31 10:12:50 -0300

    manipulation dom

---
+++ comebackToJquery/manipularDom.js
@@ -0,0 +1,29 @@
+/**
+ * append() // dentro na última posição *
+ * $("ul").append('<li>Tecnlogia append na última posição</li>')
+ */
+
+/**
+* prepend() // dentro na primeira posição
+ $("ul").prepend('<li>Tecnlogia append na primeira posição</li>')
+*/
+
+/**
+ * after() //fora e após o elemento
+ * $("ul").after('<h1>After! Fora e depois do elemento</h1>')
+*/
+
+/**
+ * before() // fora e antes do elemento
+ * $("ul").before('<h1>Before! Fora e antes do elemento</h1>')
+ */
+
+/**
+ * wrap() // envolve com outro elemento
+ * $('ul').wrap($('<p />')) // <p></p> || <p></p> || $('<p />')
+ */
+
+/**
+ * clone() // faz uma cópia de todos elementos selecionados.
+ * $('ul').append($('li').clone())
+ */
