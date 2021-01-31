3ad9f6add1bbcae24e3e72bfb602e232ed2601b9
Renan Lima <renanlimabl@gmail.com>
2021-01-31 10:33:14 -0300

    manipulando css jquery

---
+++ comebackToJquery/manipulandoCss.js
@@ -0,0 +1,19 @@
+/**
+ * .heigth()
+ * .width() pega largura atual do elemento selectionado
+ * $('img').width()
+ */
+
+/**
+ * .innerHeigth()
+ * .innerWidth() pega largura atual do elemento selecionado + padding
+ * $('img').innerWidth()
+ */
+
+/**
+ * pega a largura atual, do elemento selecionado + padding + borda
+ * $('img').outerWidth()
+ * $('img').outerWidth(true) // também pega a margin
+ * $('img').outerHeigth()
+ * $('img').outerHeigth(true) // também pega a margin
+ **/
