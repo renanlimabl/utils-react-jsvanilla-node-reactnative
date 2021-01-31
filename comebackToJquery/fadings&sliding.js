6bdb83f40b0433c18d411968184bf1c6929a8a8e
Renan Lima <renanlimabl@gmail.com>
2021-01-31 19:24:15 -0300

    add fadings e slidings

---
+++ comebackToJquery/fadings&sliding.js
@@ -0,0 +1,12 @@
+/**
+ * .fadeIn() deixa opaco. @params time, [,easing] [,cb]
+ * .fadeOut() deixa Transparente.  @params time, [,easing] [,cb]
+ * .fadeTo() atinge certa opacidade @params time, 0.7, cb
+ * .fadeToggle atige opaco e transparente @params time, [,easing] [,cb]
+ *
+ * .slideUp() fecha para cima @params time, [,easing] [,cb]
+ * .slideDown() abre pra baixo @params time, [,easing] [,cb]
+ * .slideToggle() Fecha e abre de acordo da posição @params time, [,easing] [,cb]
+ *
+ * @params normal, fast, slow, ms
+ */
