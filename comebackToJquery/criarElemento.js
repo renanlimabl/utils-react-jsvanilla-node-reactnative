b46e443345a183735893bdd5b04cc76235250f0f
Renan Lima <renanlimabl@gmail.com>
2021-01-31 09:52:54 -0300

    add como criar elementos com jquery

---
+++ comebackToJquery/criarElemento.js
@@ -0,0 +1,17 @@
+/**
+ * Adicionando uma tag IMG dentro do body
+ * const img = $('<img />, {
+ *  src: 'assets/teste.jpg',
+ *  alt: 'Imagem de teste',
+ *  click: function () {
+ *    console.log('oi')
+ *  }
+ * })
+ * $('body').append(img)
+ *
+ * Criando uma lista e adiciondo no body
+ * const ul = $("<ul />").append($("<li />").text('oi'))
+ * ou
+ * const ul = $("<ul />").append($("<li />").append('tchau'))
+ * $('body').append(ul); *
+ */
