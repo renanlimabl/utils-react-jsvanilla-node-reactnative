cbcf649ec8c6a661072ffb2e90fa9634dfb0258a
Renan Lima <renanlimabl@gmail.com>
2021-01-31 09:33:03 -0300

    add metodos de manipulacao jquery

---
+++ comebackToJquery/manipulation.js
@@ -0,0 +1,12 @@
+/**
+ * .text()
+ * pega somente o texto
+ * get: $('exemplo').text() // bla bla
+ * set: $('exemplo').text('<h1>bla</h1>') // <h1>bla</h1>
+ */
+
+/**
+ * replaceWith()
+ * Vai trocar o elemento por outro elemento, inclusive a tag selecionada.
+ * $('div').replaceWith('<p>Olá</p>')
+ */
