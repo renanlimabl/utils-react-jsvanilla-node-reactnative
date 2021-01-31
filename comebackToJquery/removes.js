5807ecce12d4cc9db25b2551a6e6d051fac9d9d2
Renan Lima <renanlimabl@gmail.com>
2021-01-31 10:18:28 -0300

    add removes methods jquery

---
+++ comebackToJquery/removes.js
@@ -0,0 +1,19 @@
+/**
+ * remove elementos alvos
+ * .remove()
+ * $('h1').remove()
+ * $('h1').remove('#id-selecionado')
+ * $('h1#id-selecionado').remove()
+ */
+
+/**
+ * limpa todos elementos interno
+ * .empty()
+ * $(ul).empty() // todas <li /> serão apagadas.
+ */
+
+/**
+ * remove elementos pais do elemento selecionado.
+ * .unwrap()
+ * $('ul').unwrap() // remove o pai envolvendo o elemento.
+ */
